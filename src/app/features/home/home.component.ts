412781206d2a1207c75e58cc4394b56e229111b9
Anh Pham <anh.is.a.dev@gmail.com>
2024-08-02 13:12:34 +0700

    refactor: move divider to component

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../shared/ui/button/button.component';
 import { RouterModule } from '@angular/router';
+import { DividerComponent } from '../../shared/ui/divider/divider.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     ButtonComponent,
-    RouterModule
+    RouterModule,
+    DividerComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
