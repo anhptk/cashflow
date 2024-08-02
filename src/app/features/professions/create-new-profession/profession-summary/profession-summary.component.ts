f4b52c7e2d22e8de559aa2382191beb0ff14a8fb
Anh Pham <anh.is.a.dev@gmail.com>
2024-08-02 17:12:31 +0700

    feat: Create new profession - calculate summary

--- src/app/features/professions/create-new-profession/profession-summary/profession-summary.component.ts
+++ src/app/features/professions/create-new-profession/profession-summary/profession-summary.component.ts
@@ -10,11 +10,34 @@ import { FormGroup } from '@angular/forms';
   styleUrl: './profession-summary.component.scss'
 })
 export class ProfessionSummaryComponent {
-  @Input() form?: FormGroup<ProfessionForm>;
+  @Input() form!: FormGroup<ProfessionForm>;
+
   professionSummary: ProfessionSummary;
 
   constructor() {
     this.professionSummary = this._buildSummary();
+  }
+
+  ngOnInit() {
+    this.form.valueChanges.subscribe((value) => {
+      this.professionSummary = this._calculateSummary(value);
+    });
+  }
+
+  private _calculateSummary(formValue: any): ProfessionSummary {
+    const salary = formValue.income.salary;
+    const passiveIncome = 0;
+    const monthlyIncome = salary;
+    const totalExpenses = formValue.expenses.taxes + formValue.expenses.homeMortgage + formValue.expenses.schoolLoan + formValue.expenses.carLoan + formValue.expenses.retail + formValue.expenses.creditCard + formValue.expenses.other;
+    const monthlyCashFlow = monthlyIncome - totalExpenses;
+
+    return {
+      salary,
+      passiveIncome,
+      monthlyIncome,
+      totalExpenses,
+      monthlyCashFlow
+    };
   }
 
   private _buildSummary(): ProfessionSummary {
