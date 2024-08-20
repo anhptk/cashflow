02a29a86322b1f8b50d64eddc30c0a43edf3df40
Anh Pham <anh.is.a.dev@gmail.com>
2024-08-21 00:09:24 +0700

    feat: payday action

--- src/app/features/actions/actions-list/actions-list.component.ts
+++ src/app/features/actions/actions-list/actions-list.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { SessionService } from '../../../shared/services/db/session.service';
+import { SessionState } from '../../../shared/models/sessions/session-state';
+import { SessionStoreService } from '../../../shared/services/stores/session-store.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-actions-list',
@@ -8,5 +12,24 @@ import { Component } from '@angular/core';
   styleUrl: './actions-list.component.scss'
 })
 export class ActionsListComponent {
+  data: SessionState;
 
+  constructor(
+    private _sessionService: SessionService,
+    private _sessionStore: SessionStoreService,
+    private _location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this._sessionStore.data$.subscribe((data) => {
+      this.data = data;
+    });
+  }
+
+  public payday(): void {
+    const cf = confirm(`Payday: Cash + $${this.data.cashflow}`);
+    if (cf) {
+      this._sessionStore.payday();
+    }
+  }
 }
