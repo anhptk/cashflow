aba07177cfdf6417003691d894f1b92e9911bf0d
To Thanh <ytominhthanh@gmail.com>
2024-08-24 07:25:55 +0000

    SessionDetailsComponent - Add delete button

--- src/app/features/sessions/session-details/session-details.component.ts
+++ src/app/features/sessions/session-details/session-details.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 import { SessionStoreService } from '../../../shared/services/stores/session-store.service';
 import { Observable } from 'rxjs';
 import { SessionState } from '../../../shared/models/sessions/session-state';
-import { CommonModule } from '@angular/common';
+import { CommonModule, Location } from '@angular/common';
 import { ProgressBarComponent } from '../../../shared/ui/progress-bar/progress-bar.component';
 import { SessionHeaderComponent } from '../widgets/session-header/session-header.component';
 import { SessionExpensesComponent } from '../widgets/session-expenses/session-expenses.component';
-
+import { ButtonComponent } from '../../../shared/ui/button/button.component';
+import { SessionService } from '../../../shared/services/db/session.service';
 @Component({
   selector: 'app-session-details',
   standalone: true,
@@ -16,7 +17,8 @@ import { SessionExpensesComponent } from '../widgets/session-expenses/session-ex
     ProgressBarComponent,
     RouterModule,
     SessionHeaderComponent,
-    SessionExpensesComponent
+    SessionExpensesComponent,
+    ButtonComponent,
   ],
   templateUrl: './session-details.component.html',
   styleUrl: './session-details.component.scss'
@@ -24,10 +26,25 @@ import { SessionExpensesComponent } from '../widgets/session-expenses/session-ex
 export class SessionDetailsComponent {
 
   data$: Observable<SessionState>;
+  sessionId: number;
 
   constructor(
     private sessionStore: SessionStoreService,
+    private sessionService: SessionService,
+    private _location: Location,
+    private route: ActivatedRoute
   ) {
     this.data$ = this.sessionStore.state$;
+    this.sessionId = +route.snapshot.params['sessionId'];
   }
+
+  public delete(): void {
+    const cf = confirm($localize`:@@confirmDeleteSession:Do you really want to delete this session?`);
+    if (cf) {
+      this.sessionService.delete(this.sessionId).subscribe(() => {
+        this._location.back();
+      });
+    }
+  }
+
 }
