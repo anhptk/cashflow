9f25309b78488eeb726785d1334d38187c25bc3a
Anh Pham <anh.is.a.dev@gmail.com>
2024-10-20 03:51:28 +0000

    update buttons UI

--- src/app/shared/constants/actions.enum.ts
+++ src/app/shared/constants/actions.enum.ts
@@ -12,6 +12,6 @@ export const ACTION_TYPE_LABEL = {
     [ACTION_TYPE.BUY]: {label: $localize`:@@buy:Buy`, color: 'green', link: 'buy'},
     [ACTION_TYPE.SELL]: {label: $localize`:@@sell:Sell`, color: 'red', link: 'sell'},
     [ACTION_TYPE.UPDATE]: {label: $localize`:@@update:Update`, color: 'blue', link: 'update'},
-    [ACTION_TYPE.SPLIT]: {label: $localize`:@@split:Split`, color: 'green', link: 'split'},
-    [ACTION_TYPE.REVERSE_SPLIT]: {label: $localize`:@@reverseSplit:Reverse Split`, color: 'red', link: 'reverse-split'}
+    [ACTION_TYPE.SPLIT]: {label: $localize`:@@split:Split`, color: 'purple', link: 'split'},
+    [ACTION_TYPE.REVERSE_SPLIT]: {label: $localize`:@@reverseSplit:Reverse Split`, color: 'gray', link: 'reverse-split'}
 } as const;
--- src/app/shared/ui/button/button.component.ts
+++ src/app/shared/ui/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, numberAttribute } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -11,4 +11,5 @@ export class ButtonComponent {
   @Input() clickFn: () => void = () => {};
   @Input() color = 'blue';
   @Input() disabled = false;
+  @Input({transform: numberAttribute}) paddingSize = 2;
 }
