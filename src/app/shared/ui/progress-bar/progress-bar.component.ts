4be2d8f2e9a814c4075a9028fb428bff6ac5aacb
Anh Pham <anh.is.a.dev@gmail.com>
2024-11-24 06:07:33 +0000

    fix: progress bar length incorrect when complete goal

--- src/app/shared/ui/progress-bar/progress-bar.component.ts
+++ src/app/shared/ui/progress-bar/progress-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, SimpleChanges } from '@angular/core';
+import { Component, Input, signal, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-progress-bar',
@@ -12,10 +12,13 @@ export class ProgressBarComponent {
   @Input() maxValue = 100;
 
   public progress = 0;
+  public completed = signal(false);
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes['value'] || changes['value'].currentValue) {
       this.progress = Math.round((this.value / this.maxValue) * 100);
+
+      this.completed.set(this.progress >= 100);
     }
   }
 }
